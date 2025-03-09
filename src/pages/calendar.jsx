e0577661c804ed668788b65ba64ffef7e1a5ebd4
Joaquin Florencio <Jrr232@gmail.com>
2025-03-08 22:04:42 -0500

    added styling to signup

--- src/pages/calendar.jsx
+++ src/pages/calendar.jsx
@@ -121,19 +121,20 @@ function Calendar() {
             <table id="calendar" bgcolor="lightgrey" align="center"
                 cellSpacing="21" cellPadding="21">
                 <caption id="table-caption" align="top">
+                    
                     <button onClick={onClickDecreaseMonth} disabled={isDisabled} >previous month</button>
                     {monthNames[Month] + " " + currentYear}
                     <button onClick={onClickIncrementMonth} >next month</button>
                 </caption>
                 <thead>
                     <tr>
-                        <th>Sun</th>
-                        <th>Mon</th>
-                        <th>Tue</th>
-                        <th>Wed</th>
-                        <th>Thu</th>
-                        <th>Fri</th>
-                        <th>Sat</th>
+                        <th id="weekdays">Sun</th>
+                        <th id="weekdays">Mon</th>
+                        <th id="weekdays">Tue</th>
+                        <th id="weekdays">Wed</th>
+                        <th id="weekdays">Thu</th>
+                        <th id="weekdays">Fri</th>
+                        <th id="weekdays">Sat</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -149,7 +150,10 @@ function Calendar() {
                     ))}
                 </tbody>
             </table>
-            <button id="next"><a href="/Services">Book Appointment</a></button>
+            <button class="addtocalendar-button"><a href="/Services">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" height="24" fill="none" class="svg-icon"><g stroke-width="2" stroke-linecap="round" stroke="#fff"><rect y="5" x="4" width="16" rx="2" height="16"></rect><path d="m8 3v4"></path><path d="m16 3v4"></path><path d="m4 11h16"></path></g></svg>
+                <span class="lable">Add to Calendar</span></a>
+            </button>
             <Hourslider id="hourslider" />
             <p id="scheduled-day">Appointment Date: {days || 'No date selected'}</p>        </>
 
