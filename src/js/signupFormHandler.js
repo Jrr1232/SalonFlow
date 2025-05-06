932dc04416d6da26f3ff573b3701243fffa2d9f4
Joaquin Florencio <Jrr232@gmail.com>
2025-05-05 23:34:27 -0400

    remove slash from env file

--- src/js/signupFormHandler.js
+++ src/js/signupFormHandler.js
@@ -21,9 +21,10 @@ const signupFormHandler = async (event, formState) => {
     const clientType = Cookies.get('client_type');
     console.log("this client type is" + clientType)
     const backendUrl = process.env.NODE_ENV === 'production'
-        ? process.env.REACT_APP_API_URL
+        ? process.env.REACT_APP_API_URL 
         : 'http://localhost:3001';
-    // console.log('Backend URL:', backendUrl)
+
+    console.log('Backend URL:', backendUrl)
     if (formState.first_name && formState.last_name) {
         try {
             const response = await fetch(`${backendUrl}/wigs`, {
